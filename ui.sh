13a1088024a55ae47de745b1e0373359079abe15
Magyar János <Janos.Magyar@oh.gov.hu>
2023-03-13 08:52:59 +0100

    create directories if not exists

--- ui.sh
+++ ui.sh
@@ -5,39 +5,39 @@
 export STRUCTURIZR_UI_DIR=../structurizr-ui
 export STRUCTURIZR_LITE_DIR=.
 
-mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/
-mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/static
+mkdir -p $STRUCTURIZR_LITE_DIR/src/main/resources/static/
+mkdir -p $STRUCTURIZR_LITE_DIR/src/main/resources/static/static
 
 # JavaScript
-mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/js
+mkdir -p $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/js
 cp $STRUCTURIZR_UI_DIR/src/js/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/js
 
 # CSS
-mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/css
+mkdir -p $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/css
 cp $STRUCTURIZR_UI_DIR/src/css/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/css
 
 # CSS fonts
-mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/css/fonts
+mkdir -p $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/css/fonts
 cp $STRUCTURIZR_UI_DIR/src/css/fonts/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/css/fonts
 
 # Images
-mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/img
+mkdir -p $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/img
 cp $STRUCTURIZR_UI_DIR/src/img/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/img
 
 # Bootstrap icons
-mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/bootstrap-icons
+mkdir -p $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/bootstrap-icons
 cp $STRUCTURIZR_UI_DIR/src/bootstrap-icons/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/bootstrap-icons
 
 # HTML (offline exports)
-mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/html
+mkdir -p $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/html
 cp $STRUCTURIZR_UI_DIR/src/html/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/html
 
 # JSP fragments
 cp $STRUCTURIZR_UI_DIR/src/fragments/* $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/fragments
 cp $STRUCTURIZR_UI_DIR/src/fragments/workspace/* $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/fragments/workspace
-mkdir $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/fragments/diagrams
+mkdir -p $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/fragments/diagrams
 cp $STRUCTURIZR_UI_DIR/src/fragments/diagrams/* $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/fragments/diagrams
-mkdir $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/fragments/decisions
+mkdir -p $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/fragments/decisions
 cp $STRUCTURIZR_UI_DIR/src/fragments/decisions/* $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/fragments/decisions
 
 # JSP
